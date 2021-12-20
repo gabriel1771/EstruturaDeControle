94a46bdbb625526f05ee9d2f1d6f4db16435f550
Gabriel <gabrielrenhardtdosreis@gmail.com>
2021-12-19 22:35:22 -0300

    lembrei do foreach

---
+++ exemplo-04.php
@@ -0,0 +1,13 @@
+<?php
+
+$messes = array(
+    "janeiro", "fevereiro", "marça", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"
+);
+
+foreach ($messes as $key => $mes) {
+    echo "indeci: " .$key . "<br>";
+    echo "este é o mes de " .$mes;
+    echo "<br> <br>";
+}
+
+?>
